05bd7871145964ccfd9437451085c6ac7fe52f5c
Ewan Burns-Wilton <ewanb97@gmail.com>
2018-09-25 19:51:56 -0500

    tutorial 3
    Code up to Pg. 12

--- app/src/main/java/com/example/ewanburns_wilton/beautifuldog/BulldogListFragment.java
+++ app/src/main/java/com/example/ewanburns_wilton/beautifuldog/BulldogListFragment.java
@@ -3,9 +3,13 @@ package com.example.ewanburns_wilton.beautifuldog;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +17,9 @@ import android.view.ViewGroup;
  */
 public class BulldogListFragment extends Fragment {
 
+    private RecyclerView bulldogList;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter bulldogAdapter;
 
     public BulldogListFragment() {
         // Required empty public constructor
@@ -25,6 +32,28 @@ public class BulldogListFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_bulldog_list, container, false);
+
+        ArrayList<Bulldog> bulldogs = new ArrayList<Bulldog>();
+        bulldogList = (RecyclerView)view.findViewById(R.id.bulldog_list);
+
+        Bulldog bulldog1 = new Bulldog();
+        bulldog1.setAge("10");
+        bulldog1.setName("Ruby");
+
+
+        Bulldog bulldog2 = new Bulldog();
+        bulldog1.setAge("1");
+        bulldog1.setName("Marley");
+
+        bulldogs.add(bulldog1);
+        bulldogs.add(bulldog2);
+
+        layoutManager = new LinearLayoutManager(getContext());
+        bulldogList.setLayoutManager(layoutManager);
+
+        bulldogAdapter = new BulldogAdapter(getContext(), bulldogs);
+        bulldogList.setAdapter(bulldogAdapter);
+
         return view;
     }
 
